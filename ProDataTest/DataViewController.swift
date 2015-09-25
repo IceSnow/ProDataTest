53ab6c10d7146b9e65ec2b8087f0a2252c97d55a
IceSnow <xinyi_yangcd@sina.com>
2015-09-25 14:21:29 +0800

    首页

---
+++ ProDataTest/DataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DataViewController.swift
+//  ProDataTest
+//
+//  Created by 王晶 on 15/9/23.
+//  Copyright © 2015年 IceSnow. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProDataTest/SQLManager.swift
@@ -0,0 +1,9 @@
+//
+//  SQLManager.swift
+//  ProDataTest
+//
+//  Created by 王晶 on 15/9/24.
+//  Copyright © 2015年 IceSnow. All rights reserved.
+//
+
+import Foundation
